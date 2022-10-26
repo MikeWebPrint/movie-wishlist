0653c8ba680c39f8ddd163a06caece660cce987c
Mike Johnson <mike@mapleprintservices.com>
2022-10-25 23:27:48 -0400

    YT api calls working on test input

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,27 @@ function fetchMovieInfo(tmdbsample){
 // https://image.tmdb.org/t/p/w500
 // or
 // https://image.tmdb.org/t/p/original
+
+var YTAPIkey = 'AIzaSyDrzmBZCuAd6fYctQJe9WsiA7sfQjFDFJA'
+// i=tt3896198&
+var searchFormTest = document.getElementById('searchFormTest')
+var movieSearchYT = document.getElementById('movie-search-boxYT')
+searchFormTest.addEventListener('submit', function fetchMovieInfoYT
+(e, movieTitle){
+  e.preventDefault();
+  var movieTitle = movieSearchYT.value;
+  var imdbId = 'tt0078788';
+  var YTsample = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&q='+ imdbId + '+'+movieTitle+'+movie+official+trailer&key='+ YTAPIkey +'&max-results=5';
+  fetch(YTsample)
+  .then(function(response){
+  return response.json();
+})
+  .then(function(data) {
+    // console.log(data)
+    var resultsYT = document.getElementById('resultsYT');
+    var YTvideolink = data.items[0].id.videoId
+    // resultsYT.innerHTML = '<p>'+ JSON.stringify(data) + '</p><a href="https://www.youtube.com/watch?v='+YTvideolink+'" target="_blank">Video Link HERE</a>'
+    resultsYT.innerHTML = '<p>'+ movieTitle + '</p><a href="https://www.youtube.com/watch?v='+YTvideolink+'" target="_blank">Video Link HERE</a>'
+    console.log(YTvideolink)
+  })
+})
