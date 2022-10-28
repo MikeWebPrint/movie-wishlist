b10070d4459ac8b319847f893741a9e76ebbc748
Mike Johnson <mike@mapleprintservices.com>
2022-10-27 20:18:01 -0400

    dummy data pulling Jaws video

--- assets/js/modules/ytDummyData.js
+++ assets/js/modules/ytDummyData.js
@@ -1,181 +1,181 @@
 export const dummyData = {
   "kind": "youtube#searchListResponse",
-  "etag": "XsP8T_uvhALn4vAVUoKRiwH2rao",
+  "etag": "3ArulgiCr4riYyij5ob5M6Pjjmc",
   "nextPageToken": "CAUQAA",
   "regionCode": "US",
   "pageInfo": {
-      "totalResults": 89,
+      "totalResults": 130,
       "resultsPerPage": 5
   },
   "items": [
       {
           "kind": "youtube#searchResult",
-          "etag": "Ac8RskL3trN31AXAZCLwVBeN_qU",
+          "etag": "aHpswc0cWixIdd3MVfq_DiNNch0",
           "id": {
               "kind": "youtube#video",
-              "videoId": "78IJdhvY1zg"
+              "videoId": "La-FBChm82o"
           },
           "snippet": {
-              "publishedAt": "2018-01-16T18:10:16Z",
-              "channelId": "UCyiKdqreDmEBJEN0cDG5OxA",
-              "title": "The Lord of the Rings (2002) -  The final Battle (Of The Hornburg) - Part 1 [4K]",
-              "description": "Well, there you go. One of the iconic battles of the millenia. It's 2002 and just look at this cgi. Freaking amazing, amiright? Gosh I ...",
+              "publishedAt": "2016-10-04T10:29:17Z",
+              "channelId": "UCWEWz6PDi00aEBmAHoYjx8Q",
+              "title": "Jaws - Trailer (1975)",
+              "description": "When a gigantic great white shark begins to menace the small island community of Amity, a police chief, a marine scientist and a ...",
               "thumbnails": {
                   "default": {
-                      "url": "https://i.ytimg.com/vi/78IJdhvY1zg/default.jpg",
+                      "url": "https://i.ytimg.com/vi/La-FBChm82o/default.jpg",
                       "width": 120,
                       "height": 90
                   },
                   "medium": {
-                      "url": "https://i.ytimg.com/vi/78IJdhvY1zg/mqdefault.jpg",
+                      "url": "https://i.ytimg.com/vi/La-FBChm82o/mqdefault.jpg",
                       "width": 320,
                       "height": 180
                   },
                   "high": {
-                      "url": "https://i.ytimg.com/vi/78IJdhvY1zg/hqdefault.jpg",
+                      "url": "https://i.ytimg.com/vi/La-FBChm82o/hqdefault.jpg",
                       "width": 480,
                       "height": 360
                   }
               },
-              "channelTitle": "Kimer Lorens",
+              "channelTitle": "Ultimate Horror",
               "liveBroadcastContent": "none",
-              "publishTime": "2018-01-16T18:10:16Z"
+              "publishTime": "2016-10-04T10:29:17Z"
           }
       },
       {
           "kind": "youtube#searchResult",
-          "etag": "pZaXbqSAuJLza65Q0VVTDByWEFI",
+          "etag": "Qg9KgUXwDMeCAqziuqR_ID_8JsI",
           "id": {
               "kind": "youtube#video",
-              "videoId": "uTYlcob5GHE"
+              "videoId": "E56hpc9chfw"
           },
           "snippet": {
-              "publishedAt": "2009-06-23T16:31:34Z",
-              "channelId": "UCMr98siV_v4OwqJHfy8HzHA",
-              "title": "The Lord of the Rings: The Two Towers (2002) Trailer",
-              "description": "Frodo and Sam continue on to Mordor in their mission to destroy the One Ring. Whilst their former companions make new allies ...",
+              "publishedAt": "2017-09-20T16:03:47Z",
+              "channelId": "UC7CEwaJk23assc-fU2A7I5A",
+              "title": "Jaws trailer - (Fanmade) HD",
+              "description": "This is an homage to Jaws (1975). In my opinion modern trailers often ruin the film by showing to much of the plot. I tried to make a ...",
               "thumbnails": {
                   "default": {
-                      "url": "https://i.ytimg.com/vi/uTYlcob5GHE/default.jpg",
+                      "url": "https://i.ytimg.com/vi/E56hpc9chfw/default.jpg",
                       "width": 120,
                       "height": 90
                   },
                   "medium": {
-                      "url": "https://i.ytimg.com/vi/uTYlcob5GHE/mqdefault.jpg",
+                      "url": "https://i.ytimg.com/vi/E56hpc9chfw/mqdefault.jpg",
                       "width": 320,
                       "height": 180
                   },
                   "high": {
-                      "url": "https://i.ytimg.com/vi/uTYlcob5GHE/hqdefault.jpg",
+                      "url": "https://i.ytimg.com/vi/E56hpc9chfw/hqdefault.jpg",
                       "width": 480,
                       "height": 360
                   }
               },
-              "channelTitle": "29march2008",
+              "channelTitle": "Stian A. Olsen",
               "liveBroadcastContent": "none",
-              "publishTime": "2009-06-23T16:31:34Z"
+              "publishTime": "2017-09-20T16:03:47Z"
           }
       },
       {
           "kind": "youtube#searchResult",
-          "etag": "L8YIDzlEZYKe1HuWqc49zg_f-2c",
+          "etag": "YlbwcEGtMNkM-448VYGti2UleEw",
           "id": {
               "kind": "youtube#video",
-              "videoId": "Lc9q6nnmrGY"
+              "videoId": "yPEFtB6Af-o"
           },
           "snippet": {
-              "publishedAt": "2018-01-16T18:18:01Z",
-              "channelId": "UCyiKdqreDmEBJEN0cDG5OxA",
-              "title": "The Lord of the Rings (2002) -  The final Battle - Part 4 - Theoden Rides Forth [4K]",
-              "description": "Well, there you go. One of the iconic battles of the millenia. It's 2002 and just look at this cgi. Freaking amazing, amiright? Gosh I ...",
+              "publishedAt": "2020-08-13T22:08:13Z",
+              "channelId": "UCogAl64H3j8CXPYP65jZqNQ",
+              "title": "Trailer - Jaws (1975)",
+              "description": "A local sheriff, a marine biologist and an old seafarer team up to hunt down a great white shark wrecking havoc in a beach resort.",
               "thumbnails": {
                   "default": {
-                      "url": "https://i.ytimg.com/vi/Lc9q6nnmrGY/default.jpg",
+                      "url": "https://i.ytimg.com/vi/yPEFtB6Af-o/default.jpg",
                       "width": 120,
                       "height": 90
                   },
                   "medium": {
-                      "url": "https://i.ytimg.com/vi/Lc9q6nnmrGY/mqdefault.jpg",
+                      "url": "https://i.ytimg.com/vi/yPEFtB6Af-o/mqdefault.jpg",
                       "width": 320,
                       "height": 180
                   },
                   "high": {
-                      "url": "https://i.ytimg.com/vi/Lc9q6nnmrGY/hqdefault.jpg",
+                      "url": "https://i.ytimg.com/vi/yPEFtB6Af-o/hqdefault.jpg",
                       "width": 480,
                       "height": 360
                   }
               },
-              "channelTitle": "Kimer Lorens",
+              "channelTitle": "Video Archive",
               "liveBroadcastContent": "none",
-              "publishTime": "2018-01-16T18:18:01Z"
+              "publishTime": "2020-08-13T22:08:13Z"
           }
       },
       {
           "kind": "youtube#searchResult",
-          "etag": "3c70bUirdyu0npUXJudidQv2kPw",
+          "etag": "7PRBj4kbAIqAaAwsBbp1WLuTZUM",
           "id": {
               "kind": "youtube#video",
-              "videoId": "8qtC-d49Du8"
+              "videoId": "tcTnr05hCFI"
           },
           "snippet": {
-              "publishedAt": "2018-04-21T10:51:34Z",
-              "channelId": "UCb7xdKzoWLjJJygSLc7Zvzg",
-              "title": "Battle of Helm&#39;s Deep Opening   The Lord of the Rings  The Two Towers 2002 CLIP 10  Subtitles",
-              "description": "The Lord of the Rings: The Two Towers (2002) (+Subtitles) -IMDb Movie Info-(Everything About This Movie): ...",
+              "publishedAt": "2020-01-13T01:21:19Z",
+              "channelId": "UCogAl64H3j8CXPYP65jZqNQ",
+              "title": "Teaser - Jaws (1975)",
+              "description": "A local sheriff, a marine biologist and an old seafarer team up to hunt down a great white shark wrecking havoc in a beach resort.",
               "thumbnails": {
                   "default": {
-                      "url": "https://i.ytimg.com/vi/8qtC-d49Du8/default.jpg",
+                      "url": "https://i.ytimg.com/vi/tcTnr05hCFI/default.jpg",
                       "width": 120,
                       "height": 90
                   },
                   "medium": {
-                      "url": "https://i.ytimg.com/vi/8qtC-d49Du8/mqdefault.jpg",
+                      "url": "https://i.ytimg.com/vi/tcTnr05hCFI/mqdefault.jpg",
                       "width": 320,
                       "height": 180
                   },
                   "high": {
-                      "url": "https://i.ytimg.com/vi/8qtC-d49Du8/hqdefault.jpg",
+                      "url": "https://i.ytimg.com/vi/tcTnr05hCFI/hqdefault.jpg",
                       "width": 480,
                       "height": 360
                   }
               },
-              "channelTitle": "Movie Clips HD",
+              "channelTitle": "Video Archive",
               "liveBroadcastContent": "none",
-              "publishTime": "2018-04-21T10:51:34Z"
+              "publishTime": "2020-01-13T01:21:19Z"
           }
       },
       {
           "kind": "youtube#searchResult",
-          "etag": "X93CFSEctYCwfzuTreReobINLUs",
+          "etag": "7v_uH2pfJQ0FyntLdMp358Yyix8",
           "id": {
               "kind": "youtube#video",
-              "videoId": "5jgq-u3z7Tc"
+              "videoId": "TplZfFdnEtQ"
           },
           "snippet": {
-              "publishedAt": "2019-07-13T18:41:25Z",
-              "channelId": "UCYNewUUyB_OXc64tT1qyUGg",
-              "title": "The Lord of the Rings: The Two Towers | Battle of Helms Deep Opening (11/12)",
-              "description": "İMDB ▻ http://www.imdb.com/title/tt0167261/",
+              "publishedAt": "2022-05-07T14:53:08Z",
+              "channelId": "UCaLMbNHEFwCKD83qflfnJXw",
+              "title": "Jaws (1975 ) - Opening scene",
+              "description": "Opening scene of movie Jaws (1975) Scene Time Duration: 1:12 to 5:5 IMDB: https://www.imdb.com/title/tt0073195/ Jaws (1975) ...",
               "thumbnails": {
                   "default": {
-                      "url": "https://i.ytimg.com/vi/5jgq-u3z7Tc/default.jpg",
+                      "url": "https://i.ytimg.com/vi/TplZfFdnEtQ/default.jpg",
                       "width": 120,
                       "height": 90
                   },
                   "medium": {
-                      "url": "https://i.ytimg.com/vi/5jgq-u3z7Tc/mqdefault.jpg",
+                      "url": "https://i.ytimg.com/vi/TplZfFdnEtQ/mqdefault.jpg",
                       "width": 320,
                       "height": 180
                   },
                   "high": {
-                      "url": "https://i.ytimg.com/vi/5jgq-u3z7Tc/hqdefault.jpg",
+                      "url": "https://i.ytimg.com/vi/TplZfFdnEtQ/hqdefault.jpg",
                       "width": 480,
                       "height": 360
                   }
               },
-              "channelTitle": "Filmorphics",
+              "channelTitle": "Gujarati Filmmaking",
               "liveBroadcastContent": "none",
-              "publishTime": "2019-07-13T18:41:25Z"
+              "publishTime": "2022-05-07T14:53:08Z"
           }
       }
   ]
